361a678b2013f573a52c6a6ac881b562c3f3f6b8
sethybur <sethburton80@gmail.com>
2025-02-21 14:43:25 -0700

    work in progress almost there

--- I2C_data_reciever/I2C_data_reciever.ino
+++ I2C_data_reciever/I2C_data_reciever.ino
@@ -1,7 +1,21 @@
 #include <Wire.h> //I2C library
-const int I2C_myAddress = 0b0000001; //7bit number identifing this device on the I2C Bus
+const uint8_t I2C_myAddress = 0b0000001; //7bit number identifing this device on the I2C Bus
 bool I2C_recievedFlag;
-char I2C_buffer[1024];
+const int bufferSize = 1024;
+char I2C_buffer[32];
+char I2C_buffer_1[bufferSize];
+char I2C_buffer_2[bufferSize];
+char I2C_buffer_4[bufferSize];
+char I2C_buffer_3[bufferSize];
+const uint8_t device1 = 0b0000010;
+const uint8_t device2 = 0b0000100;
+const uint8_t device3 = 0b0001000;
+
+int miss;
+
+unsigned long start_time;
+unsigned long end_time;
+
 
 // function headers
 void I2C_recieve();
@@ -11,39 +25,90 @@ void radioSend(char message[]);
 void setup() {
   // put your setup code here, to run once:
   
-  // initeate I2C protocol 
-  I2C_recievedFlag = false;
-  char I2C_buffer[1024] = "";
-  Wire.begin(I2C_myAddress);
-  Wire.onReceive(I2C_interupt);
+  // set up buffers
+  for (int i = 0; i < 32; i++) I2C_buffer[i] = 0;
+  I2C_buffer_1[bufferSize] = "";
+  I2C_buffer_2[bufferSize] = "";
+  I2C_buffer_3[bufferSize] = "";
+  I2C_buffer_4[bufferSize] = "";
 
   // initiate serial protocol
   Serial.begin(9600);
+
+  // initeate I2C protocol 
+  I2C_recievedFlag = false;
+  Wire.begin(I2C_myAddress);
+  Wire.onReceive(I2C_interupt);
+  miss = 0;
 
   radioSend("starting");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(10); //add delay
 
   //handle I2C comands
   if(I2C_recievedFlag) I2C_recieve();
+  if(miss > 0){
+    Serial.print("miss ");
+    Serial.println(miss);
+  }
 }
 
 /*  reads the I2C_buffer string
     sends the string to the radio if not empty
     */
 void I2C_recieve() {
+    
+    // select buffer
+    char *buffer;
+    uint8_t device = I2C_buffer[0];
+    if (device = device1) buffer = I2C_buffer_1;
+    if (device = device2) buffer = I2C_buffer_2;
+    if (device = device3) buffer = I2C_buffer_3;
+      Serial.println(I2C_buffer);
+      int i = 0;
+      while (buffer[i] != 0) i++;
+      Serial.println(i, DEC);
+      int j = 1;
+      while (I2C_buffer[j] != 0 && j < 32) {
+        buffer[i] = I2C_buffer[j];
+        i++;
+        j++;
+      }
 
-  if (I2C_buffer != "") {
-    radioSend(I2C_buffer);
-    int i = 0;
-    while (I2C_buffer[i] != '\0') {
-      I2C_buffer[i] = '\0';
-      i++;
-    } 
-  }
+
+    // //get to blank character
+    // int i = 0;
+    
+    // 
+   
+    // for (int j = 1; i < 32; i++){
+    //   buffer[i] = I2C_buffer[j];
+    //   i++;
+    //   // if mesage is over send to radio and clear buffer
+    //   if (I2C_buffer[j] == 0){
+    //     if (buffer != "") {
+    //       radioSend(I2C_buffer);
+    //       int i = 0;
+    //       while (buffer[i] != 0) {
+    //         buffer[i] = 0;
+    //         i++;
+    //       } 
+    //     }
+    //     break;
+    //   }
+    // }
+
+      // Serial.println(buffer);
+      // i = 0;
+      // while (buffer[i] != 0 && i < bufferSize) {
+      //   buffer[i] = 0;
+      //   i++;
+      // }
+
+
+  for (int i = 0; i < 32; i++) I2C_buffer[i] = 0;
   I2C_recievedFlag = false; //set flag to false
 }
 
@@ -53,6 +118,7 @@ void I2C_recieve() {
     stores as string I2C_buffer 
     */
 void I2C_interupt(int number_of_bytes) {
+
   for(int i = 0; i < number_of_bytes; i++)
   {
     I2C_buffer[i] = Wire.read();
--- I2C_data_senders/I2C_data_senders.ino
+++ I2C_data_senders/I2C_data_senders.ino
@@ -1,6 +1,6 @@
 #include <Wire.h> //I2C library
-const int I2C_myAddress = 0b1000000; //7bit number identifing this device on the I2C Bus
-const int I2C_controllerAddress = 0b0000001; //7bit number identifing where data should be sent to
+const uint8_t I2C_myAddress = 0b0000100; //7bit number identifing this device on the I2C Bus
+const uint8_t I2C_controllerAddress = 0b0000001; //7bit number identifing where data should be sent to
 bool I2C_recievedFlag;
 char I2C_buffer[1024];
 
@@ -11,26 +11,26 @@ void I2C_interupt(int number_of_bytes);
 
 void setup() {
   // put your setup code here, to run once:
-  
   // initeate I2C protocol 
   I2C_recievedFlag = false;
-  char I2C_buffer[1024] = "";
+  I2C_buffer[1024] = "";
   Wire.begin(I2C_myAddress);
   Wire.onReceive(I2C_interupt);
 
-  randomSeed(I2C_myAddress);
+    // initiate serial protocol
+  Serial.begin(9600);
 
+  randomSeed(I2C_myAddress);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(100); //add delay
-
-  //simulating sending data
   delay(1000);
+  //simulating sending data
   String num = String(random(10), DEC);
   String mesage = "sensor id: " + String(I2C_myAddress) + ":\t" + num;
-  I2C_send(mesage.c_str());
+  // I2C_send(mesage.c_str());
+  I2C_send("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbcccccccccccccccccccccccccccccccdddddddddddddddddddddddddddddddeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeefffffffffffffffffffffffffffffffggggggggggggggggggggggggggggggghhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiijjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkklllllllllllllllllllllllllllllllmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnooooooooooooooooooooooooooooooopppppppppppppppppppppppppppppppqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrssssssssssssssssssssssssssssssstttttttttttttttttttttttttttttttuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz11111111111111111111111111111112222222222222222222222222222222333333333333333333333333333333344444444444444444444444444444445555555555555555555555555555555666666666666666666666666666666677777777777777777777777777777778");
 
   //handle I2C comands
   if(I2C_recievedFlag) I2C_recieve();
@@ -41,9 +41,26 @@ void loop() {
     functions like println() but to controller
     */
 void I2C_send(char message[]) {
-  Wire.beginTransmission(I2C_controllerAddress); //reserves the bus for transmitting
-  Wire.write(message); //writes the bytes to the bus
-  Wire.endTransmission(); //unreserves the bus
+  int index = 0;
+  int bytes_sent = 0;
+  while(message[index] != 0) {
+    Wire.beginTransmission(I2C_controllerAddress); //reserves the bus for transmitting
+
+    // sends my address so reader knows where this is coming from.
+    Wire.write(I2C_myAddress);
+    bytes_sent++;
+
+    // send the mesage in 32 byte chunks
+    while(message[index] != 0 && bytes_sent < 32) {
+      Wire.write(message[index]);
+      
+      index++;
+      bytes_sent++;
+    }
+    Wire.endTransmission(); //unreserves the bus
+    delay(500);
+    bytes_sent = 0;
+  }
 }
 
 /*  reads the I2C_buffer string 
